e2f01f824c51f8b7b5006d95f8aac1d7c773ee83
Freaki-N <87651878+Freaki-N@users.noreply.github.com>
2021-12-13 22:51:00 +0100

    Benutzerdefinierte Werte und Grundprogramm aufgeteilt.

---
+++ ESP32_Datenuebertragung/values.cpp
@@ -0,0 +1,37 @@
+//created by Freaki_N
+//Last Update: 13.12.2021
+
+const int PROTOCOL_INPUT_PIN = 25;
+const int PROTOCOL_OUTPUT_PIN = 27;
+
+const int device_id = 1;
+
+const int LED_PIN_1 = 21;
+const int BUTTON_PIN_1 = 22;
+
+boolean Button_1_state = false;
+boolean Button_1_toggle = false;
+
+void readData(int _sender, int _packet){
+  if(_packet == 2){
+    digitalWrite(LED_PIN_1, HIGH);
+  }else if(_packet == 3){
+    digitalWrite(LED_PIN_1, LOW);
+  }
+}
+
+void loop2(){
+  if((digitalRead(BUTTON_PIN_1) == LOW) && (digitalRead(BUTTON_PIN_1) == LOW) != Button_1_state){
+    Button_1_toggle = !Button_1_toggle;
+    if (Button_1_toggle) {
+      dataToSend.add(Datagramm(device_id, 0, 2));
+    }else{
+      dataToSend.add(Datagramm(device_id, 0, 3));
+    }
+  }
+
+  if(!(digitalRead(BUTTON_PIN_1) == LOW) && (digitalRead(BUTTON_PIN_1) == LOW) != Button_1_state){
+
+  }
+  Button_1_state = (digitalRead(BUTTON_PIN_1) == LOW);
+}

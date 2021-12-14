42b0e51ab438d54ec6267b9d0d95c07e48b2ab21
Freaki-N <87651878+Freaki-N@users.noreply.github.com>
2021-12-14 10:16:54 +0100

    Update ESP32_Datenuebertragung.ino

--- ESP32_Datenuebertragung/ESP32_Datenuebertragung.ino
+++ ESP32_Datenuebertragung/ESP32_Datenuebertragung.ino
@@ -282,42 +282,6 @@ class DatagrammList{
     boolean isAvailable();
     Datagramm getNext();
   private:
-    Datagramm dataSend[MAX_DATAGRAMMS_WAITING];
-};
-
-DatagrammList::DatagrammList(){
-  for(int i=0; i < MAX_DATAGRAMMS_WAITING; i++){
-    dataSend[i] = Datagramm();
-  }
-}
-
-void DatagrammList::add(Datagramm newDatagramm){
-  boolean added = false;
-  for(int i=0; i < MAX_DATAGRAMMS_WAITING; i++){
-    if((!dataSend[i].hasData()) && (!added)){
-      dataSend[i] = newDatagramm;
-      added = true;
-    }else if(added){
-      dataSend[i] = Datagramm();
-    }
-  }
-}
-
-Datagramm DatagrammList::getNext(){
-  if(isAvailable()){
-    Datagramm data = dataSend[0];
-    for(int i=1; i < MAX_DATAGRAMMS_WAITING; i++){
-      dataSend[i-1] = dataSend[i];
-    }
-    return data;
-  }else{
-    return Datagramm();
-  }
-}
-
-boolean DatagrammList::isAvailable(){
-  return dataSend[0].hasData();
-}
 
 Datagramm receivedData = Datagramm();
 Datagramm sendingData = Datagramm();

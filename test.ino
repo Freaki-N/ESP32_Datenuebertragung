186d5bdd3e4934e73dd6976caa2047cd7b6313d4
Freaki-N <87651878+Freaki-N@users.noreply.github.com>
2021-12-14 12:34:36 +0100

    Update test.ino

--- test.ino
+++ test.ino
@@ -8,5 +8,8 @@ void setup(){
 }
 
 void loop(){
-  
+  if(!(digitalRead(BUTTON_PIN_1) == LOW) && (digitalRead(BUTTON_PIN_1) == LOW) != Button_1_state){
+    Button_1_toggle = !Button_1_toggle;
+  }
+  Button_1_state = (digitalRead(BUTTON_PIN_1) == LOW);
 }
